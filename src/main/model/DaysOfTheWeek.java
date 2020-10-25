1178a7e75cb6cce32a6370aff47c8b346f9f4681
Joe Bu <a4g3b@ugrad.cs.ubc.ca>
2020-10-25 13:10:17 -0600

    Phase 2 added tests

--- src/main/model/DaysOfTheWeek.java
+++ src/main/model/DaysOfTheWeek.java
@@ -1,6 +1,6 @@
 package model;
 
-//Represents the days of the week
+//Represents the days of the week with corresponding int values
 public enum DaysOfTheWeek {
     
     Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
